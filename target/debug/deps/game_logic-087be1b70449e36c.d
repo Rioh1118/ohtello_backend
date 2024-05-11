dd05384245b18b9e7b36eb01c8f799e910287a22
Rioh1118 <rio2515t@gmail.com>
2024-05-11 23:56:20 +0900

    ammend

--- target/debug/deps/game_logic-087be1b70449e36c.d
+++ target/debug/deps/game_logic-087be1b70449e36c.d
@@ -1,8 +1,9 @@
-/Users/riohatta/RustroverProjects/othello_api/target/debug/deps/libgame_logic-087be1b70449e36c.rmeta: game_logic/src/lib.rs Cargo.toml
+/Users/riohatta/RustroverProjects/othello_api/target/debug/deps/libgame_logic-087be1b70449e36c.rmeta: game_logic/src/lib.rs game_logic/src/state.rs Cargo.toml
 
-/Users/riohatta/RustroverProjects/othello_api/target/debug/deps/game_logic-087be1b70449e36c.d: game_logic/src/lib.rs Cargo.toml
+/Users/riohatta/RustroverProjects/othello_api/target/debug/deps/game_logic-087be1b70449e36c.d: game_logic/src/lib.rs game_logic/src/state.rs Cargo.toml
 
 game_logic/src/lib.rs:
+game_logic/src/state.rs:
 Cargo.toml:
 
 # env-dep:CLIPPY_ARGS=
